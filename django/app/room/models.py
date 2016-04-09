16dc38347a59fb152c1dd180b74891021a1b3fa3
leesangyoon <pinetree408@gmail.com>
2016-04-09 18:44:55 +0900

    Add room model to creator user

--- django/app/room/models.py
+++ django/app/room/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Room(models.Model):
 
     name = models.CharField(max_length=200)
+    creator = models.ForeignKey(User)
     created_at = models.DateTimeField(u"created at", auto_now_add=True)
 
     class Meta:
